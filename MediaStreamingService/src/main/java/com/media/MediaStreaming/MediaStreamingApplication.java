058589e1dffa6c55b82391a79f406ec20013b884
shovan <karnashovan2@email.com>
2024-11-16 11:43:10 +0530

    Inital Commit, dependency only added

---
+++ MediaStreamingService/src/main/java/com/media/MediaStreaming/MediaStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.media.MediaStreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaStreamingApplication.class, args);
+	}
+
+}
---
+++ MediaStreamingService/src/test/java/com/media/MediaStreaming/MediaStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.MediaStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
