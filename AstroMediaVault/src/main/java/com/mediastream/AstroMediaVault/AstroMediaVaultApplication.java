0840bb24867c9de5ff3c58b8d53ec1042e706863
shovan <karnashovan2@email.com>
2025-02-16 16:59:55 +0530

    New bas e codes

---
+++ AstroMediaVault/src/main/java/com/mediastream/AstroMediaVault/AstroMediaVaultApplication.java
@@ -0,0 +1,13 @@
+package com.mediastream.AstroMediaVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroMediaVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroMediaVaultApplication.class, args);
+	}
+
+}
---
+++ AstroMediaVault/src/test/java/com/mediastream/AstroMediaVault/AstroMediaVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediastream.AstroMediaVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroMediaVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
